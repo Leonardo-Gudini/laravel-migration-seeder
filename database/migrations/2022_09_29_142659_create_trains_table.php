d41bcc9bf6eb7f1033f50afff0fca2c7756a6d3f
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-29 16:51:47 +0200

    add trains table

---
+++ database/migrations/2022_09_29_142659_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->char('company', 50);
+            $table->char('departure_station', 50);
+            $table->char('arrival_station', 50);
+            $table->time('departure_time', 0);
+            $table->time('arrival_time', 0);
+            $table->smallInteger('train_code');
+            $table->tinyInteger('train_wagon');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
